04628bd9bc8f501d9e2cc4ca652b223257024873
SanhoLee <stlsh0516@gmail.com>
2020-10-17 20:55:17 +0900

    1.3.1 search feature, split function and something else

---
+++ global.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#define delimeter ","
+#define FILE_NAME "phone-number.csv"
+
+typedef struct
+{
+    /* data */
+    char name[20];
+    char pNumber[20];
+} person;
+
+#endif
--- main.c
+++ main.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h> // malloc, free가 포함된 라이브러리
 #include <string.h>
-#define FILE_NAME "phone-number.csv"
+#include "global.h"
 
-typedef struct
-{
-    /* data */
-    char name[20];
-    char pNumber[20];
-} person;
+int add_person();
+person *splitString(char *originalString);
 
 int main()
 {
-    int userChoice;
+    char userChoice;
     int num;
     int i;
 
     while (1)
     {
-        printf("\n*----- mini phonebook program -----*\n");
+        printf("*----- mini phonebook program -----*\n");
         printf("*-----          menu          -----*\n\n");
         printf("1. add \n");
         printf("2. search \n");
@@ -27,44 +23,27 @@ int main()
         printf("5. showAll \n");
         printf("9. EXIT \n");
         printf("\nSelect ? ");
-        scanf("%d", &userChoice);
+        scanf("%c", &userChoice);
+        getchar();
         printf("\n");
 
-        if (userChoice == 1)
+        if (userChoice == '1')
         {
-            /* add_person function
-            in : name(str), phone number(str)
-            out : write the info on csv file.
-            */
-            person *p1 = malloc(sizeof(person));
-            printf("\n NAME : ");
-            scanf("%s", p1->name);
-            printf(" PHONE NUMBER : ");
-            scanf("%s", p1->pNumber);
-
-            // 파일 입출력, csv file format
-            FILE *fp;
-            fp = fopen(FILE_NAME, "a");
-
-            if (fp == NULL)
-            {
-                printf("Write Error \n");
-                return 0;
-            }
-            fputs(p1->name, fp);
-            fputs(",", fp);
-            fputs(p1->pNumber, fp);
-            fputs("\n", fp);
-            free(p1);
-            fclose(fp);
+            add_person();
         }
-        else if (userChoice == 2)
+        else if (userChoice == '2')
         {
+            /* search function.
+            get data by name or phone number.
+
+            */
             char searchBy;
-            getchar();
+            char term[20] = "";
+            char singleLine[40] = "";
             printf("\n*----- search -----* \n\n");
             printf("( 1. name ?  2. Phone Number ? C : Cancel ) : ");
             scanf("%c", &searchBy);
+            getchar();
             if ((searchBy == 'c') || (searchBy == 'C'))
             {
                 printf("\n Canceled...\n");
@@ -73,7 +52,26 @@ int main()
             else if (searchBy == '1')
             {
                 // searching by name.
-                printf("\n searching by name...\n");
+                printf("\n searching by name : ");
+                scanf("%s", term);
+                getchar();
+
+                FILE *fp;
+                fp = fopen(FILE_NAME, "r");
+                if (fp == NULL)
+                {
+                    printf("No file on specified diretory. \n");
+                    return 0;
+                }
+
+                fgets(singleLine, 40, fp);
+                person *p21 = splitString(singleLine);
+                printf("\ncheck read on struct elemet ---+ \n");
+                printf("name: %s\n", p21->name);
+                printf("number: %s\n", p21->pNumber);
+
+                free(p21);
+                fclose(fp);
             }
             else if (searchBy == '2')
             {
@@ -82,21 +80,21 @@ int main()
             }
             else
             {
-                printf("\n Input Error. You should select from specified number or C(cancel) \n");
+                printf("Input Error. You should select from specified number or C(cancel) \n");
                 continue;
             }
         }
-        else if (userChoice == 3)
+        else if (userChoice == '3')
         {
             printf("update");
         }
-        else if (userChoice == 4)
+        else if (userChoice == '4')
         {
             printf("delete");
         }
-        else if (userChoice == 5)
+        else if (userChoice == '5')
         {
-            printf("\n*----- showAll -----* \n\n");
+            printf("*----- showAll -----* \n\n");
             FILE *fp;
             fp = fopen(FILE_NAME, "r");
             if (fp == NULL)
@@ -116,7 +114,7 @@ int main()
             printf("\n\nLIST END\n");
             fclose(fp);
         }
-        else if (userChoice == 9)
+        else if (userChoice == '9')
         {
             printf("***** PROGRAM END ***** \n");
             break;
@@ -133,4 +131,61 @@ int main()
         userChoice = 0;
     };
     return 0;
+}
+
+int add_person()
+{
+    /* 
+    add_person function
+    in : name(str), phone number(str)
+    out : write the info on csv file.
+    */
+    person *p1 = malloc(sizeof(person));
+    printf("\n NAME : ");
+    scanf("%s", p1->name);
+    getchar();
+    printf(" PHONE NUMBER : ");
+    scanf("%s", p1->pNumber);
+    getchar();
+
+    // 파일 입출력, csv file format
+    FILE *fp;
+    fp = fopen(FILE_NAME, "a");
+
+    if (fp == NULL)
+    {
+        printf("Write Error \n");
+        return 1;
+    }
+    fputs(p1->name, fp);
+    fputs(",", fp);
+    fputs(p1->pNumber, fp);
+    fputs("\n", fp);
+    free(p1);
+    fclose(fp);
+
+    return 0;
+}
+
+person *splitString(char *originalString)
+{
+    int i = 0;
+
+    person *p = malloc(sizeof(person));
+    char *result;
+    char *strArr[2];
+
+    result = strtok(originalString, delimeter);
+    while (result != NULL)
+    {
+        strArr[i] = result;
+        result = strtok(NULL, delimeter);
+        i++;
+    }
+
+    // save on struct address by using array elemet.
+    strcpy(p->name, strArr[0]);
+    strcpy(p->pNumber, strArr[1]);
+
+    return p;
 }
